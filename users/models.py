25c7731dd9ac37805ee991ec05a9c1b376ddd388
Kirsten Baldon <Kurstinshin@users.noreply.github.com>
2026-07-08 13:43:28 +0800

    add User model

--- users/models.py
+++ users/models.py
@@ -4,12 +4,13 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     ROLE_CHOICES = [
-        ("user", "User"),
-        ("owner", "Owner"),
+        ("landlord", "Landlord"),
+        ("tenant", "Tenant"),
     ]
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="user")
+    contact_number = models.CharField(max_length=30, blank=True)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="tenant")
 
     def __str__(self):
         return f"{self.user.email} ({self.role})"
